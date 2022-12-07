706a71d567b0ca228823ca6062731e8b9e0510e9
Abubakar Muradov <muradov.abubakar999@gmail.com>
2022-12-07 16:19:24 +0400

    Конфигурация Spring Boot и базовые настройки

---
+++ src/main/java/ru/zig199/stockstore/StockStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.zig199.stockstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zig199/stockstore/StockStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zig199.stockstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
